a0c7a0109068ee46a7345364732c5b4d07638348
Greg <yadig69design@gmail.com>
2025-09-12 23:15:58 -0400

    alab_316 1, 2: working on toggle and add and remove active

--- js/app.js
+++ js/app.js
@@ -60,13 +60,13 @@ mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 mainEl.classList.add('flex-ctr');
 
-console.log(mainEl)
-
+// console.log(mainEl)
+// topMenuEl styling
 const topMenuEl = document.getElementById('top-menu');
 topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 topMenuEl.classList.add('flex-around');
-console.log(topMenuEl)
+// console.log(topMenuEl)
 
 // Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
 
@@ -84,7 +84,9 @@ for(let menuLink of menuLinks) {
     console.log(link)
 }
 
-const subMenuEl = document.getElementById('sub-menu');
+// Part 3: Creating the Submenu
+
+const subMenuEl = document.getElementById('sub-menu',);
 subMenuEl.style.height = '100%';
 subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
 subMenuEl.classList.add('flex-around');
@@ -92,14 +94,66 @@ subMenuEl.classList.add('flex-around');
 subMenuEl.style.position = 'absolute';
 subMenuEl.style.top = '0';
 
-// Part 4
+// Part 4 Adding Menu Interaction
 
-const topMenuLinks = topMenuEl.querySelectorAll('a');
-function handleClick(event) {
-    event.preventDefault();
+const topMenuLinks = topMenuEl.querySelectorAll('#top-menu a');
+console.log(topMenuLinks)
+let showMenu = false;
 
 
-}
+ topMenuEl.addEventListener('click', function (event) {
+// 	// call the event object's preventDefault() method.
+ 	event.preventDefault();
+	 // 	// immediately return if the element clicked was not an <a> element.
+	 const link = event.target.textContent;
+ 	if (event.target.textContent !== 'a') return;
+	 console.log(link)
+		 
+	 if (link.classList.contains('active')) {
+		link.classList.remove('active');
+		subMenuEl.style.top = '0';
+	 }
+
+	 topMenuLinks.forEach(function (link) {
+		link.classList.remove('active');
+	 });
+		
+	 link.classList.add('active');
+
+	 const linkData = menuLinks.find(function (linkObject) {
+		return linkObject.text === link;
+	 });
+	 console.log(linkData)
+	 if (linkData.subLinks) {
+		 showMenu = true;
+		 subMenuEl.style.top = '100%';
+
+	 } else {
+		 subMenuEl.style.top = '0';
+		showMenu = false;
+	 }
+		
+	})
+	//	The event listener should add the active class to the <a> element that was clicked, unless it was already active, in which case it should remove it.
+// 	topMenuLinks.forEach(function (link) {
+// 	link.addEventListener('click', function (event) {
+// 		event.preventDefault();
+// 		if (event.target.textContent !== 'a') {
+// 			return;
+// 		} else {
+// 			event.target.classList.toggle('active',);
+// 		}
+// 	});
+// } )
+	
+	// The event listener should remove the active class from each other <a> element in topMenuLinks - whether the active class exists or not.
+	// Hint: Removing a non-existent class from an element does not cause an error!
+
+//});
+// Ensure that clicking ABOUT, CATALOG, etc. logs about, catalog, etc. when a link is clicked. Clicking anywhere other than on a link should do nothing.
+
+
+
+console.log(a)
 console.log(topMenuLinks);
-
 console.log(subMenuEl)
